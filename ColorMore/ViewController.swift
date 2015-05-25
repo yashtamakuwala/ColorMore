606ace09683134cd8371cb3779ab2e1806917bdb
YASH <yash@YASHs-MacBook-Pro.local>
2015-05-25 21:26:57 +0530

    Initial Commit

---
+++ ColorMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMore
+//
+//  Created by YASH on 25/05/15.
+//  Copyright (c) 2015 YASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
