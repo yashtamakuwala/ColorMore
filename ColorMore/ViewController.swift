6fdecb4b55d89b2fc4a902ae5c90385ed4b3013e
YASH <yash@YASHs-MacBook-Pro.local>
2015-05-26 19:06:59 +0530

    The colors are mixed in the box depending on whether its switch is toggled on and its opacity.

--- ColorMore/ViewController.swift
+++ ColorMore/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+ {
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeColorComponent(sender : AnyObject)
+    {
+        let r: CGFloat = self.redControl.on ? 1 : 0
+        let g: CGFloat = self.greenControl.on ? 1 : 0
+        let b: CGFloat = self.blueControl.on ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green : g, blue: b, alpha :1)
     }
 
 
